8c343807598965122869ac31f89e32cd7e144c82
Syifa Dwiky Basamala <119727680+syifadwikyb@users.noreply.github.com>
2024-03-20 12:21:44 +0700

    Update tugas1.java

--- java/com/example/home/tugas1.java
+++ java/com/example/home/tugas1.java
@@ -9,18 +9,43 @@ import android.widget.ImageButton;
 
 public class tugas1 extends AppCompatActivity {
 
-    ImageButton imageButton;
+    private ImageButton imageButton,list,keuangan,profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tugas1);
 
-        ImageButton imageButton = findViewById(R.id.imageButton);
+        imageButton = findViewById(R.id.imageButton);
+        list = findViewById(R.id.list);
+        keuangan = findViewById(R.id.keuangan);
+        profile = findViewById(R.id.profile);
+        
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showPopup();
+            }
+        });
+        list.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (tugas1.this, tugas1.class);
+                startActivity(intent);
+            }
+        });
+        keuangan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (tugas1.this, keuangan.class);
+                startActivity(intent);
+            }
+        });
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (tugas1.this, profile.class);
+                startActivity(intent);
             }
         });
     }
